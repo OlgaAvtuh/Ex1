bc33ec67618548ead1c44322e73f75dde6d91dad
Coco-S <coco-s@list.ru>
2018-01-15 09:43:52 +0300

    First coqwmm

--- exggg/exggg/Program.cs
+++ exggg/exggg/Program.cs
@@ -1,11 +1,33 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+
 
 namespace exggg
 {
+    class Button
+    {
+        public string _caption;
+        public string _color;
+
+        public int x { get; set; }
+        public int y { get; set; }
+
+        public void _point(int x, int y)
+        {
+            this.x = x;
+            this.y = y;
+        }
+
+        public override string ToString() => _color;
+        public int _size;
+    }
+    class RadioButton : Button
+    {
+        enum state
+        {checkedd, uncheked }
+
+       
+
+    }
     class Program
     {
         static void Main(string[] args)
